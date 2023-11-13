6fb1a1f2b2fa423dc01de56daf2ee99a8529976d
Marcos Carvalho <marcosfcar@gmail.com>
2023-11-13 19:58:32 +0000

    Create structure for the pages

---
+++ channelcacheClient/src/app/components/app/header-toolbar/header-toolbar.component.ts
@@ -0,0 +1,13 @@
+import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+@Component({
+  selector: 'app-header-toolbar',
+  standalone: true,
+  imports: [CommonModule],
+  templateUrl: './header-toolbar.component.html',
+  styleUrl: './header-toolbar.component.scss'
+})
+export class HeaderToolbarComponent {
+
+}
---
+++ channelcacheClient/src/app/components/app/sidebar-menu/sidebar-menu.component.ts
@@ -0,0 +1,13 @@
+import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+@Component({
+  selector: 'app-sidebar-menu',
+  standalone: true,
+  imports: [CommonModule],
+  templateUrl: './sidebar-menu.component.html',
+  styleUrl: './sidebar-menu.component.scss'
+})
+export class SidebarMenuComponent {
+
+}
