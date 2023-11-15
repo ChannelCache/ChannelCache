5b2a9a4347472247f3133aa2ffb38d7462cbbd2c
Marcos Carvalho <marcosfcar@gmail.com>
2023-11-15 19:33:34 +0000

    Create structure for the pages

--- channelcacheClient/src/app/app.component.ts
+++ channelcacheClient/src/app/app.component.ts
@@ -1,11 +1,12 @@
 import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { RouterOutlet } from '@angular/router';
+import {AppContainerComponent} from "./components/app/app-container/app-container.component";
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [CommonModule, RouterOutlet],
+  imports: [CommonModule, RouterOutlet, AppContainerComponent],
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
--- channelcacheClient/src/app/app.routes.ts
+++ channelcacheClient/src/app/app.routes.ts
@@ -1,4 +1,4 @@
-import { Routes } from '@angular/router';
+import {Route, Routes} from '@angular/router';
 import {HomeComponent} from "./pages/home/home.component";
 import {ChannelsComponent} from "./pages/channels/channels.component";
 import {PlaylistsComponent} from "./pages/playlists/playlists.component";
